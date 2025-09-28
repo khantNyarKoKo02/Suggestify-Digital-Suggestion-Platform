aa625fc07668e0bc511a59bfcb5ccf5ff807a061
Khant Nyar Ko Ko <112542063+Khant-Nyar-Ko-Ko@users.noreply.github.com>
2025-09-28 21:55:40 +0700

    feat: config-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,20 @@
     build: {
       target: 'esnext',
       outDir: 'build',
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('react')) return 'react';
+              if (id.includes('@radix-ui')) return 'radix';
+              if (id.includes('recharts')) return 'recharts';
+              if (id.includes('@supabase')) return 'supabase';
+              return 'vendor';
+            }
+          },
+        },
+      },
     },
     server: {
       host: '127.0.0.1',
